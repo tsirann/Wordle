e3ba84f38f95feeaf7bb344089bb5e0699f80e25
tsirann <ssaskee@inbox.ru>
2025-05-09 20:12:20 +1000

    update code

--- console_wordle.py
+++ console_wordle.py
@@ -1,66 +1,71 @@
 from random import choice
 
-# Присваиваем в переменную все 5 буквенные слова
+# Присваиваем переменной список пятибуквенных слов
 with open('words5.txt', 'r', encoding='utf-8') as file:
-    words_list = file.readlines()
+    words_list = [word.rstrip() for word in file.readlines()]
 
-# Проверка совпадений букв с загаданным словом
-def word_exam(word) -> str:
-    ls = []
+# Цвета
+green = '\033[32m'
+yellow = '\033[33m'
+white = '\033[37m'
+red = '\033[31m'
+blue = '\033[36m'
+
+def word_exam(word, correct_word) -> str:
+    """Проверяет слово на совпадения букв и возвращает раскрашенное слово"""
+    colored_letters = []
     for i in range(5):
         if word[i] == correct_word[i]:
-            ls.append(f'\033[32m{word[i]}')
+            colored_letters.append(green + word[i])
         elif word[i] in correct_word:
-            ls.append(f'\033[33m{word[i]}')
+            colored_letters.append(yellow + word[i])
         else:
-            ls.append(f'\033[37m{word[i]}')
-    return ' '.join(ls) + '\033[37m'
+            colored_letters.append(white + word[i])
+    return ' '.join(colored_letters) + white
 
-# Основной модуль игры
 def wordle_game() -> bool:
+    """Основной цикл игры Wordle"""
     input_list = ['□ □ □ □ □' for _ in range(6)]
-    # Присваиваем переменной рандомное слово, которое нужно будет отгадать
-    global correct_word
-    correct_word = (choice(words_list)).rstrip()
+    correct_word = (choice(words_list)).rstrip() # Присваиваем переменной рандомное слово, которое нужно будет отгадать
 
     # Цикл обрабатывающий все 6 попыток ввода
     for i in range(6):
 
-        print(' \033[37mWordle')
+        print(f' {white}Wordle')
         print(*input_list, sep='\n')
-        print('''\033[31mПодсказка: \033[37mA - буквы нет в слове; \033[33mА \033[37m- буква есть в слове, но не на своем месте; \033[32mА \033[37m- буква есть в слове и на своем месте.
-\033[36mВведите слово:\033[37m''')
+        print(f'''{red}Подсказка: {white}A - буквы нет в слове; {yellow}А {white}- буква есть в слове, но не на своем месте; {green}А {white}- буква есть в слове и на своем месте.
+{blue}Введите слово:{white}''')
 
         # c - вспомогательная переменная, цикл пока не будет введено подходящее слово
-        c = 0
-        while c == 0:
+        is_valid_input = 0
+        while is_valid_input == 0:
             word_input = input().lower()
-            if word_input.isalpha() and len(word_input) == 5 and (word_input + '\n') in words_list:
-                input_list[i] = word_exam(word_input)
-                c = 1
+            if word_input.isalpha() and len(word_input) == 5 and word_input in words_list:
+                input_list[i] = word_exam(word_input, correct_word)
+                is_valid_input = 1
             else:
-                print('Неизвестное слово! Попробуйте еще раз:')
+                print(f'{red}Неизвестное слово! {blue}Попробуйте еще раз:{white}')
 
-        # Проверка на соответсвие слова с загаданным
+        # Проверка на соответствие слова с загаданным
         if word_input == correct_word:
-            print(' \033[37mWordle')
+            print(f' {white}Wordle')
             print(*input_list, sep='\n')
-            print('''\033[32mВы победили!\033[37m
-Хотите сыграть еще раз? Y/N: ''')
-            if input() == 'N':
-                return False
-            return True
+            print(f'''{green}Вы победили!{white}
+Хотите сыграть еще раз? {green}Y{white}/{red}N{white}: ''')
+            if input().upper() == 'Y':
+                return True
+            return False
 
     # Если цикл не вернул ничего, значит игрок не отгадал слово и тем самым проиграл
     else:
-        print(' \033[37mWordle')
+        print(f' {white}Wordle')
         print(*input_list, sep='\n')
-        print(f'''\033[31mВы проиграли!\033[37m
-Загаданное слово: {correct_word}
-Хотите сыграть еще раз? Y/N: ''')
-        if input() == 'N':
-            return False
-        return True
+        print(f'''{red}Вы проиграли!
+{blue}Загаданное слово: {green}{correct_word}{white}
+Хотите сыграть еще раз? {green}Y{white}/{red}N{white}: ''')
+        if input().upper() == 'Y':
+            return True
+        return False
 
 # Цикл для повторения игр и для ее запуска
 while True:
